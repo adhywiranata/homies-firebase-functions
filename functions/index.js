9212289cb01ecc36ef2d145dd14011074cde56bd
Adhy <adhywiranatap@gmail.com>
2017-07-05 09:53:50 +0700

    update properties api functions

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,17 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const admin = require('firebase-admin');
-
 admin.initializeApp(functions.config().firebase);
+
 const app = express();
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello from Express on Firebase!');
+});
+
+app.get('/properties', (req, res) => {
+  return admin.database().ref('/properties').once('value', snapshot => {
+    res.send(JSON.stringify(snapshot.val()));
+  });
 });
 
 exports.api = functions.https.onRequest(app);
